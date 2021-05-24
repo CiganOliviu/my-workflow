f406b47ba79b5c6411163db84e7cb8585aaf30cc
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-05-24 23:18:21 +0300

    Setup models and migrations

--- myworkflow/projects/models.py
+++ myworkflow/projects/models.py
@@ -1,10 +1,30 @@
 from django.db import models
 
 
+class DevelopmentStack(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class ComputerScienceProjects(models.Model):
-    pass
+    name = models.CharField(max_length=50)
+    details = models.TextField()
+    stack = models.ForeignKey(DevelopmentStack, on_delete=models.CASCADE)
+    github_url = models.URLField(max_length=200)
+    start_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
 
 
 class UniversityProjects(models.Model):
-    pass
+    name = models.CharField(max_length=50)
+    details = models.TextField()
+    start_date = models.DateTimeField()
+    deadline = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
 
