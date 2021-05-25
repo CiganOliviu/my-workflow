abebb92f4106f9f1a2bf7b5e9caf0b2c3ae6ff15
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-05-25 22:00:50 +0300

    Update admin.py

--- myworkflow/projects/admin.py
+++ myworkflow/projects/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from .models import *
 
+
+class PersonalProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'stack', 'start_date')
+
+
+class PersonalFinishedProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'stack')
+
+
+class UniversityProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'deadline')
+
+
 admin.site.register(DevelopmentStack)
 admin.site.register(PersonalProject)
 admin.site.register(PersonalFinishedProject)
 admin.site.register(UniversityProject)
+admin.site.register(UniversityClasses)
 
 admin.site.site_header = "MyWorkflow"
