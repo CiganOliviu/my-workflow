d76c9a5079fc52ce565d347c9bc9a4c7115d54e0
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-05-25 22:26:41 +0300

    Update urls.py

--- myworkflow/projects/urls.py
+++ myworkflow/projects/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('restapi/development-stack/<int:pk>', views.DevelopmentStackDetails.as_view(), name='DevelopmentStackDetails'),
     path('restapi/personal-projects/', views.PersonalProjectsLister.as_view(), name='DevelopmentStackLister'),
     path('restapi/personal-projects/<int:pk>', views.PersonalProjectsDetails.as_view(), name='DevelopmentStackDetails'),
+    path('restapi/personal-finished-projects/', views.PersonalFinishedProjectLister.as_view(), name='PersonalFinishedProjectLister'),
     path('restapi/university-projects/', views.UniversityProjectLister.as_view(), name='DevelopmentStackLister'),
     path('restapi/university-projects/<int:pk>', views.UniversityProjectsDetails.as_view(), name='DevelopmentStackDetails'),
 ]
