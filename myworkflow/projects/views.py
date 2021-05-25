5d4201f9bb41f6436eb2bf1982ccf16c4d18a2b6
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-05-25 22:31:11 +0300

    Create PersonalFinishedProjectDetails

--- myworkflow/projects/views.py
+++ myworkflow/projects/views.py
@@ -139,7 +139,34 @@ class PersonalFinishedProjectLister(APIView):
 
 
 class PersonalFinishedProjectDetails(APIView):
-    pass
+
+    def get_post(self, pk):
+        try:
+            return PersonalFinishedProject.objects.get(pk=pk)
+        except:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        post = self.get_post(pk)
+        serializer = PersonalFinishedProjectSerializer(post)
+
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        project = self.get_post(pk)
+        serializer = PersonalFinishedProjectSerializer(project, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        project = self.get_post(pk)
+        project.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class UniversityProjectLister(APIView):
