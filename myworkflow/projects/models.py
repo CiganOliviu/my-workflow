897562411bc27879f8048cb66e045115d781d647
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-06-20 21:13:21 +0300

    Update models.py

--- myworkflow/projects/models.py
+++ myworkflow/projects/models.py
@@ -24,6 +24,17 @@ class PersonalProject(models.Model):
         return self.name
 
 
+class Organization(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=25)
+    gmail = models.CharField(max_length=50)
+    description = models.TextField()
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class UniversityProject(models.Model):
     name = models.CharField(max_length=50)
     details = models.TextField()
