56392a7d6bc598ba119e1ef52a53d750de4743e1
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-06-22 23:02:15 +0300

    Update views.py

--- myworkflow/projects/views.py
+++ myworkflow/projects/views.py
@@ -268,8 +268,8 @@ class CurrentReadingBooksDetails(APIView):
 class OrganizationsLister(APIView):
 
     def get(self, request, format=None):
-        organization = CurrentReadingBook.objects.all()
-        serializer = CurrentReadingBooksSerializer(organization, many=True)
+        organization = Organization.objects.all()
+        serializer = OrganizationsSerializer(organization, many=True)
 
         return Response(serializer.data)
 
@@ -285,4 +285,32 @@ class OrganizationsLister(APIView):
 
 
 class OrganizationsDetails(APIView):
-    pass
+
+    def get_post(self, pk):
+        try:
+            return Organization.objects.get(pk=pk)
+        except:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        post = self.get_post(pk)
+        serializer = OrganizationsSerializer(post)
+
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        organization = self.get_post(pk)
+        serializer = OrganizationsSerializer(organization, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        organization = self.get_post(pk)
+        organization.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
