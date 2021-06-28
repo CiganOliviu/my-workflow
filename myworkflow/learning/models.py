a8d95176e670ddd4d0eb89a9c2df83e47edfd2ba
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-06-28 22:59:11 +0300

    Update databases and models

--- myworkflow/learning/models.py
+++ myworkflow/learning/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class CurrentReadingBook(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
