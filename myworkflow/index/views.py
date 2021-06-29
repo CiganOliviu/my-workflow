20614d15520652963b96682f27dfc27b4b6d397e
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-06-29 23:20:32 +0300

    Update views.py and setup templates

--- myworkflow/index/views.py
+++ myworkflow/index/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h1>TemplateView is coming</h1>')
+    return render(request, template_name='views/index.html')
